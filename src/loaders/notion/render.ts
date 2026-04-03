6aefdfe51da7847d9c13e822156c9afad233a30d
Jason Lengstorf <jason@codetv.dev>
2026-04-02 19:11:45 -0700

    revert: just kidding

--- src/loaders/notion/render.ts
+++ src/loaders/notion/render.ts
@@ -56,7 +56,7 @@ async function renderImage(
     const upload = await cloudinary.uploader.upload(imageUrl, {
       overwrite: false,
       use_filename: true,
-      unique_filename: true,
+      unique_filename: false,
     });
 
     imageUrl = cloudinary.url(upload.public_id, {
